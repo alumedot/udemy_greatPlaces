782b4960f8f21e1648f06462c1b5e70bb8f618a8
Alexey Kozlov <alumedot@icloud.com>
2020-06-25 11:36:44 +0300

    PlacesListScreen.tsx component

--- src/screens/PlacesListScreen.tsx
+++ src/screens/PlacesListScreen.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { useSelector } from 'react-redux';
+import { StyleSheet, Platform, FlatList } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { IRooReduxState } from '../store/types';
+import PlaceItem from '../components/PlaceItem';
 
 import HeaderButton from '../components/HeaderButton';
 
 const PlacesListScreen = props => {
+  const places = useSelector((state: IRooReduxState) => state.places.places);
+
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <PlaceItem
+          image={''}
+          title={itemData.item.title}
+          address={''}
+          onSelect={() => props.navigation.navigate(
+            'PlaceDetail',
+            {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            },
+          )}
+        />
+      )}
+    />
   );
 };
 
---
+++ src/store/types.ts
@@ -0,0 +1,5 @@
+import { TPlacesState } from './places';
+
+export interface IRooReduxState {
+  places: TPlacesState;
+}
