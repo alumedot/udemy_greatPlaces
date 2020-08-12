f955d6012dadf8470e4ca8ae69b091bb49dd1e0f
Alexey Kozlov <alumedot@icloud.com>
2020-08-12 08:34:54 +0300

    Marker on the map

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker, MapEvent } from 'react-native-maps';
 
-const MapScreen = props => {
+const MapScreen = () => {
+  const [ selectedLocation, setSelectedLocation ] = useState<{ lat: number, lng: number } | null>(null);
   const mapRegion = {
     latitude: 44.68395800531082,
     longitude: 37.78696593213832,
@@ -10,8 +11,32 @@ const MapScreen = props => {
     longitudeDelta: 0.0421,
   };
 
+  const onSelectLocation = (e: MapEvent) => {
+    setSelectedLocation({
+      lat: e.nativeEvent.coordinate.latitude,
+      lng: e.nativeEvent.coordinate.longitude,
+    })
+  };
+
+  let markerCoordinates;
+
+  if (selectedLocation) {
+    markerCoordinates = {
+      latitude: selectedLocation.lat,
+      longitude: selectedLocation.lng,
+    }
+  }
+
   return (
-    <MapView style={styles.map} region={mapRegion} />
+    <MapView
+      style={styles.map}
+      region={mapRegion}
+      onPress={onSelectLocation}
+    >
+      {
+        markerCoordinates && <Marker title="Picked Location" coordinate={markerCoordinates} />
+      }
+    </MapView>
   );
 };
 
